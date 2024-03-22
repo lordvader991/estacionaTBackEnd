d418034f23bdafa403061d344b5b93722f1fe675
denzel fuentes <denzel.fuentes.nunez@gmail.com>
2024-03-22 06:43:55 -0400

    add gitignore

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +20,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sex-^l=u6mj2mtp$m#66#w&6@0rcz03b)n@=l0%d6wirw1s5@x'
+SECRET_KEY = os.environ.get('SECRET_KEY',default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
+RENDER_EXTERNAL_HOSTNEM  = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+
+if RENDER_EXTERNAL_HOSTNEM:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNEM)
+
+
 APPEND_SLASH = False
 
 
