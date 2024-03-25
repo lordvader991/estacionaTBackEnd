b4dcdeef285f8552c6b3dfa5c22ee0336c55feb9
denzel fuentes <denzel.fuentes.nunez@gmail.com>
2024-03-25 13:04:00 -0400

    update setting

--- demo/settings.py
+++ demo/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     'accounts',
     'vehiculos',
     'parqueo',
@@ -62,7 +63,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
 
 ROOT_URLCONF = 'demo.urls'
 
