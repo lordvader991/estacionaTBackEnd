bdad4be9baa8cd04e853f3bfd596d2bf005ae087
Jose_Maria <josemariazapataq2@gmail.com>
2024-03-28 23:40:56 -0400

    arreglo del rol_usuario

--- accounts/views.py
+++ accounts/views.py
@@ -16,13 +16,13 @@ class LoginAPIView(APIView):
     def get(self, request):
         serializer = UserSerializer(User.objects.all(), many=True)
         return Response(status=status.HTTP_200_OK, data=serializer.data)
-    
+
     def post(self,req):
         user = get_object_or_404(User,username = req.data['username'])
 
         if not user.check_password(req.data['password']):
             return Response({"error":"invalid password"},status = status.HTTP_404_BAD_REQUEST)
-        
+
         token, created = Token.objects.get_or_create(user=user)
         serializer = UserSerializer(instance=user)
 
@@ -41,7 +41,8 @@ class SignupAPIView(APIView):
                 username=user_data['username'],
                 last_name=user_data['last_name'],
                 email=user_data['email'],
-                phone=user_data['phone']
+                phone=user_data['phone'],
+                rol_usuario=user_data.get('rol_usuario', False)
             )
             user.set_password(user_data['password'])
             user.correo_auth = True  #cuando envia el correo ahi se vuelve true
@@ -52,8 +53,8 @@ class SignupAPIView(APIView):
             return Response({'token': token.key, 'user': user_data}, status=status.HTTP_201_CREATED)
 
     def send_confirmation_email(self, to_email):
-        sender_email = settings.EMAIL_HOST_USER  
-        sender_password = settings.EMAIL_HOST_PASSWORD  
+        sender_email = settings.EMAIL_HOST_USER
+        sender_password = settings.EMAIL_HOST_PASSWORD
         #mensaje
         subject = "¡Registro exitoso!"
         body = "¡Gracias por registrarte en nuestra aplicación EstacionaT!"
@@ -66,7 +67,7 @@ class SignupAPIView(APIView):
             server.starttls()
             server.login(sender_email, sender_password)
             server.send_message(message)
-        
+
 class UserDetailApiView(APIView):
     def get_object(self, pk):
         try:
@@ -98,4 +99,4 @@ class UserDetailApiView(APIView):
         user.delete()
         response_data = {'deleted': True}
         return Response(status=status.HTTP_200_OK, data=response_data)
-    
+
