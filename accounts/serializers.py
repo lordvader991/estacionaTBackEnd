5863af9ff365e9d036b8255a849b05aaea466675
Jose_Maria <josemariazapataq2@gmail.com>
2024-04-05 17:07:06 -0400

    cambios

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -3,6 +3,7 @@ from accounts.models import  User
 
 
 class UserSerializer(ModelSerializer):
+    password = serializers.CharField(write_only=True, required=False)
     class Meta:
         model = User
         fields = ['id','username', 'last_name', 'email','password', 'phone','rol_usuario','correo_auth']
