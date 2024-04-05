22fe947b5806bb85ab266b597abe7a2f22edad90
Jose_Maria <josemariazapataq2@gmail.com>
2024-04-05 17:35:42 -0400

    lo que sea

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,10 +1,16 @@
-from rest_framework.serializers import ModelSerializer
-from accounts.models import  User
+from rest_framework import serializers
+from accounts.models import User
 
-
-class UserSerializer(ModelSerializer):
-    password = serializers.CharField(required=False)
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(required=False, write_only=False)
     class Meta:
         model = User
-        fields = ['id','username', 'last_name', 'email','password', 'phone','rol_usuario','correo_auth']
-        #fields = '__all__'
+        fields = ['id', 'username', 'last_name', 'email', 'password', 'phone', 'rol_usuario', 'correo_auth']
+    extra_kwargs = {
+        'password': {'write_only': True},
+        'email': {'required': True},
+    }
+    def update(self,instance, validated_data):
+        if 'passowrd' in validated_data:
+            password = validated_data.pop('password')
+        return super().update(instance, validated_data)
