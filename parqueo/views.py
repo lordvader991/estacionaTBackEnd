0ca42b3e0c06a95dd48673fbbf5f918af626dc04
Jose_Maria <josemariazapataq2@gmail.com>
2024-04-16 11:12:56 -0400

    roles de usuario

--- parqueo/views.py
+++ parqueo/views.py
@@ -10,11 +10,16 @@ class ParkingApiView(APIView):
     def get(self, request):
         serializer = ParkingSerializer(Parking.objects.all(), many=True)
         return Response(status=status.HTTP_200_OK, data=serializer.data)
-    def post(self,request):
-        serializer=ParkingSerializer(data=request.data)
+
+    def post(self, request):
+        serializer = ParkingSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        user_id = serializer.validated_data.get('user')
+
         serializer.save()
+        user = serializer.validated_data.get('user')
+        user.rol_usuario = True
+        user.save()
+
         return Response(status=status.HTTP_201_CREATED, data=serializer.data)
 
 class ParkingDetailApiView(APIView):
